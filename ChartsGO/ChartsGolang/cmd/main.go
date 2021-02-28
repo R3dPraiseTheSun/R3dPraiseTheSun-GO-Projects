f558999318175d54ac30e353b5dbf2869fdd3781
Paul <pa.ionescu@yahoo.com>
2021-02-28 19:00:20 +0200

    uPlot bars update

--- ChartsGO/ChartsGolang/cmd/main.go
+++ ChartsGO/ChartsGolang/cmd/main.go
@@ -10,30 +10,45 @@ import (
 var tpl *template.Template
 
 func main() {
+	Run()
+}
+
+func Run() {
 	ConfigChart := charts.Cfg{
-		ChartType:         1,
+		ChartType:         0,
 		ChartLegend:       "LineExample1",
-		TemplatePath:      "../resources/templates/line.gohtml",
+		TemplatePath:      "../resources/templates/template.gohtml",
 		JSLibrary:         "../../resources/libs/",
-		ScreenCapturePath: "../Examples/HTMLFile/example1",
+		ScreenCapturePath: "../Examples/HTMLFile/",
 	}
 	Series := charts.Series{
-		Name:   "LineChartExample",
+		Name:   "Line Example 1",
 		DataOX: []interface{}{100, 200, 210, 400, 500},
 		DataOY: []interface{}{325, 420, 102, 300, 444},
 	}
 	Dataset := charts.ChartDataset{
 		Series,
 	}
+	Chart := charts.NewChart(ConfigChart, Dataset)
 	var wg sync.WaitGroup
 	wg.Add(1)
+	Chart.Render(&wg)
+
+	wg.Add(1)
 	charts.NewChart(
-		charts.NewChartConfiguration(1, "LineExample2", "../resources/templates/line.gohtml", "../../resources/libs/", "../Examples/HTMLFile/example2"),
-		charts.NewChartDataset("Line", []interface{}{100, 200, 300, 400, 500, 600, 700, 800, 900, 1000}, []interface{}{325, 420, 200, 111, 450, 555, 666, 455, 980, 100}),
+		charts.NewChartConfiguration(0, "LineExample2", "../resources/templates/template.gohtml", "../../resources/libs/", "../Examples/HTMLFile/"),
+		charts.NewChartDataset("Line Example 2", []interface{}{100, 200, 300, 400, 500, 600, 700, 800, 900, 1000}, []interface{}{325, 420, 200, 111, 450, 555, 666, 455, 980, 100}),
+	).Render(&wg)
+	wg.Add(1)
+	charts.NewChart(
+		charts.NewChartConfiguration(1, "BarsExample1", "../resources/templates/template.gohtml", "../../resources/libs/", "../Examples/HTMLFile/"),
+		charts.NewChartDataset("Bars Example 1", []interface{}{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}, []interface{}{325, 420, 200, 111, 450, 555, 666, 455, 980, 100}),
+	).Render(&wg)
+	wg.Add(1)
+	charts.NewChart(
+		charts.NewChartConfiguration(1, "BarsExample2", "../resources/templates/template.gohtml", "../../resources/libs/", "../Examples/HTMLFile/"),
+		charts.NewChartDataset("Bars Example 2", []interface{}{100, 200, 300, 400, 500, 600, 700}, []interface{}{980, 450, 200, 111, 420, 555, 666, 455, 325, 100}),
 	).Render(&wg)
 
-	Chart := charts.NewChart(ConfigChart, Dataset)
-	wg.Add(1)
-	Chart.Render(&wg)
 	wg.Wait()
 }
---
+++ ChartsGO/ChartsGolang/cmd/t_main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestRun(t *testing.T) {
+
+}
+
+func BenchmarkRun(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Run()
+	}
+}
